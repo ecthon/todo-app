d4b4e4d0fde5a012dd31cad379fe9dc7ca44bf06
newecthon <ecthon@gmail.com>
2023-06-30 15:55:17 -0400

    Add nav items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,61 +12,7 @@ export default function Home() {
       align='center'
     >
       <Header />
-      <Flex
-        w='928px'
-        h='400px'
-        direction='column'
-        bg='gray.701'
-        mt='64px'
-        borderRadius='10px'
-        p='28px'
-      >
-        <Flex
-          justifyContent='space-between'
-          w='100%'
-          mb='32px'
-        >
-          <Text
-          fontSize='24px'
-          fontWeight='semibold'
-          color='white'
-          >
-            Title
-          </Text>
-          <Text color='white'>NOVO PROJETO</Text>
-        </Flex>
-
-        <Flex
-          w='100%'
-          h='140px'
-          bg='gray.901'
-          borderRadius='10px'
-          align='center'
-          p='18px'        
-        >
-          <Flex
-            direction='column'
-          >
-            <Text
-              fontSize='24px'
-              fontWeight='semibold'
-              color='gray.101'
-              mb='16px'
-            >
-              Lorem Ipsum Dolor
-            </Text>
-            <Text
-              fontSize='16px'
-              color='gray.301'
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis impedit repellendus quod rem voluptatem.
-            </Text>
-          </Flex>
-
-          <Text color='red'>...</Text>
-
-        </Flex>
-      </Flex>  
+      
     </Flex>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
-import { Box, Flex,Image, Text } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex,Image, Text } from "@chakra-ui/react";
 
 export function Header() {
     return (
         <Flex as='header'
+            direction='column'
             w='100%'
             h='280px'
             align='center'
-            bg='gray.801'
+            bg='#16161A'
         >
             <Flex
                 w='1120px'
+                h='106px'
                 margin='0 auto'
                 justifyContent='space-between'
+                align='center'
+                borderBottom='1px solid #202025'
+                mb='32px'
             >
                 <Image src='/assets/logo.svg' alt="" />
                 <Flex>
@@ -31,6 +36,78 @@ export function Header() {
                             src="https://github.com/ecthon.png" 
                         />
                 </Flex>
+            </Flex>
+
+            <Flex
+                justifyContent='space-between'
+                w='1120px'
+            >
+                <Flex
+                    gap='48px'
+                >
+                    <Flex
+                        gap='16px'
+                        align='center'
+                    >
+                        <Text color='white' fontSize='14px' fontWeight='semibold'>PROJETOS</Text>
+                        <Text
+                            as='span'
+                            p='2px 8px'
+                            borderRadius='full'
+                            bg='#414146'
+                            color='white'
+                            fontSize='14px'
+                            fontWeight='semibold'
+                        >
+                            10
+                        </Text>
+                    </Flex>
+                    <Flex
+                        gap='16px'
+                        align='center'
+                    >
+                        <Text color='white' fontSize='14px' fontWeight='semibold'>PROJETOS</Text>
+                        <Text
+                            as='span'
+                            p='2px 8px'
+                            borderRadius='full'
+                            bg='#414146'
+                            color='white'
+                            fontSize='14px'
+                            fontWeight='semibold'
+                        >
+                            10
+                        </Text>
+                    </Flex>
+                    <Flex
+                        gap='16px'
+                        align='center'
+                    >
+                        <Text color='white' fontSize='14px' fontWeight='semibold'>PROJETOS</Text>
+                        <Text
+                            as='span'
+                            p='2px 8px'
+                            borderRadius='full'
+                            bg='#414146'
+                            color='white'
+                            fontSize='14px'
+                            fontWeight='semibold'
+                        >
+                            10
+                        </Text>
+                    </Flex>
+                </Flex>
+
+                <Button
+                    padding='12px 32px'
+                    borderRadius='full'
+                    bg='#E9A800'
+                    fontWeight='semibold'
+                    fontSize='14px'
+                    color='white'
+                    >
+                        NOVO PROJETO
+                    </Button>
             </Flex>
         </Flex>
     )
